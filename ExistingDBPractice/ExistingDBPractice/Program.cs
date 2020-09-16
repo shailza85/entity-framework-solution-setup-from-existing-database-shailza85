5e3c66929e974d7acc60f0bbe09ff0fd966c5ae3
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-16 17:56:51 -0600

    Added database query to select person's full name.

--- ExistingDBPractice/ExistingDBPractice/Program.cs
+++ ExistingDBPractice/ExistingDBPractice/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using ExistingDBPractice.Models;
 
 namespace ExistingDBPractice
 {
@@ -6,7 +9,34 @@ namespace ExistingDBPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Take in a first name from the user
+                Case insensitive
+                Trimmed
+                Then print the full name of that user from the database
+                If the name is not in the database then output “Name not found.” and exit.
+
+             */
+
+            using (PersonContext context = new PersonContext())
+            {
+                string firstName;
+                Console.Write("Please enter the First Name: ");
+                firstName = Console.ReadLine().ToLower().Trim();
+
+                string fname;
+                if (context.Person.Any(x => x.FirstName == firstName))
+                {
+                  fname = context.Person.Where(x => x.FirstName == firstName).Select(x => x.FirstName + " " + x.LastName).FirstOrDefault().ToString();
+
+                    Console.WriteLine("The full name of person is:" + fname);
+                }
+                else                  
+                {
+                    Console.WriteLine("Name not found.");
+                }  
+
+            }
         }
     }
 }
