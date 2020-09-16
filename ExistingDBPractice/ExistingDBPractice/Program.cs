aec0f670836a89668ed2576e3e3aec8ce5bcced9
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-16 15:57:10 -0600

    Initial commit.

---
+++ ExistingDBPractice/ExistingDBPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExistingDBPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
